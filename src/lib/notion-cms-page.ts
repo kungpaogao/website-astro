0695f60dc02c046f163ea2025fb24960fbf8e2b9
kungpaogao <kungpaogao@gmail.com>
2025-03-16 17:24:11 -0400

    Delete about and move Notion IDs to env file

--- src/lib/notion-cms-page.ts
+++ src/lib/notion-cms-page.ts
@@ -6,34 +6,6 @@ import { ensureFullResponse } from "./notion-cms";
 import { parseProperty } from "./notion-parse";
 import notion from "./notion-client";
 
-/**
- * It takes a Notion URL and returns the properly formatted page id. This is
- * important because it allows the human-readable/accessible URL to be placed
- * in the corresponding file.
- *
- * For example, any of the following:
- *
- * - https://www.notion.so/kungpaogao/about-bd0d4055c64d47f0bb0c01160ce7239e
- * - about-bd0d4055c64d47f0bb0c01160ce7239e
- * - bd0d4055c64d47f0bb0c01160ce7239e
- *
- * should become: `bd0d4055-c64d-47f0-bb0c-01160ce7239e`
- *
- * @param {string} url - The URL of the page you want to get the ID of
- * @returns A string
- */
-export function parsePageUrl(url: string): string {
-  // https://www.notion.so/kungpaogao/about-bd0d4055c64d47f0bb0c01160ce7239e?v=something
-  const cleanSlash = url.split("/");
-  // about-bd0d4055c64d47f0bb0c01160ce7239e?v=something
-  const cleanParams = cleanSlash[cleanSlash.length - 1].split("?");
-  // about-bd0d4055c64d47f0bb0c01160ce7239e
-  const cleanDash = cleanParams[0].split("-");
-  const sl = (start?: number, end?: number) =>
-    cleanDash[cleanDash.length - 1].slice(start, end);
-  return `${sl(0, 8)}-${sl(8, 12)}-${sl(12, 16)}-${sl(16, 20)}-${sl(20)}`;
-}
-
 export async function getPageProperties(pageId: string): Promise<any> {
   const response = await notion.pages.retrieve({ page_id: pageId });
   const fullResponse = ensureFullResponse<
