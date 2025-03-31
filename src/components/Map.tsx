020e4e7f7148ab3c34da7b63691274e6e8ea506e
Andrew Gao <kungpaogao@gmail.com>
2025-03-31 00:45:41 -0400

    Update layout so maps can be full width + navigation is fixed

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -8,17 +8,14 @@ interface MapProps {
 
 const Map: Component<MapProps> = ({ places }) => {
   onMount(() => {
-    console.log("Map component");
-    // leaflet stuff
+    // NYC coordinates to center map
     const latitude = 40.74900042010468;
     const longitude = -73.98575388499262;
+    // init leaflet
     const map = L.map("map").setView([latitude, longitude], 11);
+    // set map tiles
     const googleStreetTiles =
       "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}";
-    const googleHybridTiles =
-      "http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}";
-    const osmTiles = "https://tile.openstreetmap.org/{z}/{x}/{y}.png";
-
     L.tileLayer(googleStreetTiles, {
       maxZoom: 19,
       subdomains: ["mt0", "mt1", "mt2", "mt3"],
@@ -26,6 +23,7 @@ const Map: Component<MapProps> = ({ places }) => {
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
+    // add markers for each place
     places.forEach((place) => {
       L.marker([place.latitude, place.longitude])
         .addTo(map)
@@ -34,8 +32,9 @@ const Map: Component<MapProps> = ({ places }) => {
   });
 
   return (
-    <div>
-      <div id="map" class="h-[400px] w-full" />
+    <div class="flex-1">
+      <noscript>Unfortunately, you need JavaScript to see the map :^(</noscript>
+      <div id="map" class="h-full w-full" />
     </div>
   );
 };
