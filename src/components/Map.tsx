43628a2e82aa7085e04217f5a1b87a3ce7f095bb
Andrew Gao <kungpaogao@gmail.com>
2025-04-07 01:14:01 -0400

    Map sheets and popup concepts

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,35 @@
 import L from "leaflet";
-import { onMount, type Component } from "solid-js";
+import {
+  createSignal,
+  onCleanup,
+  onMount,
+  Show,
+  type Component,
+} from "solid-js";
+import { render } from "solid-js/web";
 import type { Place } from "../lib/places";
+import { format, set } from "date-fns";
 
 interface MapProps {
   places: Place[];
 }
 
+const Popup: Component<{ place: Place }> = ({ place }) => {
+  return (
+    <div class="bg-white">
+      <p class="m-0 text-gray-500">This is a popup for {place.name}.</p>
+      <p class="m-0 text-gray-500">This is a popup for {place.name}.</p>
+      <p class="m-0 text-gray-500">This is a popup for {place.name}.</p>
+      <p class="m-0 text-gray-500">This is a popup for {place.name}.</p>
+    </div>
+  );
+};
+
 const Map: Component<MapProps> = ({ places }) => {
+  const [map, setMap] = createSignal<L.Map | null>(null);
+  const [selectedPlace, setSelectedPlace] = createSignal<Place | null>(null);
+  const [viewport, setViewport] = createSignal(null);
+
   onMount(() => {
     // NYC coordinates to center map
     const latitude = 40.74900042010468;
@@ -23,9 +46,23 @@ const Map: Component<MapProps> = ({ places }) => {
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 
+    // set map events
+    map.on("click", () => {
+      onClickMap();
+    });
+
     // add markers for each place
     places.forEach((place) => {
+      const popupContainer = L.DomUtil.create("div", "solid-popup-container");
+      const popup = L.popup({
+        closeButton: false,
+      }).setContent(popupContainer);
+
+      // render solid component in popup
+      render(() => <Popup place={place} />, popupContainer);
+
       L.marker([place.latitude, place.longitude], {
+        // custom icons and shadows
         icon: L.icon({
           iconUrl: "assets/pin.png",
           iconSize: [25, 25],
@@ -35,14 +72,50 @@ const Map: Component<MapProps> = ({ places }) => {
         }),
       })
         .addTo(map)
-        .bindPopup(place.name);
+        .on("click", () => {
+          // custom click behavior
+          onClickMarker(place);
+        })
+        .bindPopup(popup);
     });
+
+    setMap(map);
+  });
+
+  function onClickMarker(place: Place) {
+    setSelectedPlace(place);
+    // if sheet is going to cover the marker, recenter the remaining map area around the selected place
+  }
+
+  function onClickMap() {
+    setSelectedPlace(null);
+  }
+
+  onCleanup(() => {
+    // remove map
+    map()?.remove();
   });
 
   return (
-    <div class="flex-1">
+    <div class="tailwind-leaflet relative isolate flex-1">
       <noscript>Unfortunately, you need JavaScript to see the map :^(</noscript>
       <div id="map" class="h-full w-full" />
+      <Show when={selectedPlace()}>
+        <Sheet place={selectedPlace} />
+      </Show>
+    </div>
+  );
+};
+
+const Sheet: Component<{ place: () => Place }> = (props) => {
+  return (
+    <div class="prose absolute top-0 right-0 left-1/2 z-[1000] m-3 rounded-md bg-stone-100 px-5 pb-3 shadow-2xl">
+      <h2>{props.place().name}</h2>
+      <p>{props.place().address}</p>
+      <p>{props.place().notes}</p>
+      <p class="text-gray-400">
+        Added on {format(new Date(props.place().timestampMs), "yyyy-MM-dd")}
+      </p>
     </div>
   );
 };
